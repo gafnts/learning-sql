6fbaec300842bf2dbe9d646ae1ed0dd3a9da7619
gafnts <gafnts@gmail.com>
2022-07-28 12:14:22 -0600

    Chapter 07

--- chapter_07.sql
+++ chapter_07.sql
@@ -87,6 +87,89 @@ SELECT SUBSTRING('goodbye cruel world', 9, 5);
 
 -- Working with numeric data
 
+SELECT (37*59) / (78 - (8*6));
+
+SELECT MOD(10, 4);
+
+SELECT 
+    POW(2, 10) kilobyte,
+    POW(2, 20) megabyte,
+    POW(2, 30) gigabyte,
+    POW(2, 40) terabyte;
+
+-- Controlling number precision
+
+SELECT CEIL(72.000000001), FLOOR(72.999999999);
+
+SELECT ROUND(72.49999), ROUND(72.5), ROUND(72.50001);
+
+SELECT ROUND(72.49999, 1), ROUND(72.5, 2), ROUND(72.50001, 3);
+
+SELECT TRUNCATE(72.0909, 1), TRUNCATE(72.0909, 2), TRUNCATE(72.0909, 3);
+
+SELECT ROUND(17, -1), TRUNCATE(17, -1);
+
+-- Handling signed data
+
+SELECT amount, SIGN(amount), ABS(amount)
+FROM payment;
+
+-- Working with temporal data
+
+SELECT @@global.time_zone, @@session.time_zone;
+
+SELECT time_zone = 'Europe/Zurich';
+
+-- Generating temporal data
+
+UPDATE rental
+SET return_date = '2022-09-17 15:30:00'
+WHERE rental_id = 99999;
+
+SELECT CAST('2022-09-17 15:30:00' AS DATETIME) Date;
+
+SELECT 
+    CAST('2022-09-17 15:30:00' AS DATE) date_field,
+    CAST('108:17:57' AS TIME) time_field;
+
+UPDATE rental
+SET return_date = STR_TO_DATE('September 17, 2019', '%M %d, %Y')
+WHERE rental_id = 1;
+
+SELECT * FROM rental WHERE rental_id = 1;
+
+SELECT CURRENT_DATE(), CURRENT_TIME(), CURRENT_TIMESTAMP();
+
+-- Manipulating temporal data
+
+SELECT DATE_ADD(CURRENT_DATE(), INTERVAL 5 DAY);
+
+UPDATE actor
+SET last_update = DATE_ADD(last_update, INTERVAL '9-10' YEAR_MONTH)
+WHERE actor_id = 1;
+
+SELECT * FROM actor WHERE actor_id = 1;
+
+SELECT DAYNAME(last_update) FROM actor WHERE actor_id = 1;
+
+SELECT EXTRACT(YEAR FROM last_update) FROM actor WHERE actor_id = 1;
+
+SELECT 
+    customer_id AS id,
+    create_date, last_update,
+    DATEDIFF(last_update, create_date) AS diff
+FROM customer;
+
+-- Conversion functions
+
+SELECT CAST('123456789' AS SIGNED INTEGER);
+
+SELECT CAST('1234ABC56789' AS UNSIGNED INTEGER);
+
+
+
+
+
 
 
 
